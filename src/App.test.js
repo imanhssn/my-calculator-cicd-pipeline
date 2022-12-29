74782b38753003d22ed19d691716966f737ba0ec
Iman Hassan <imanhssn7@gmail.com>
2022-12-29 18:15:10 +0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the landing page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('calculator returns correct value', () => {
+    const { getByTestId, getByText } = render(<App />);
+    const counterValueEl = getByTestId("counter-value");
+    const oneButtonEl = getByText("1", { selector: "button" });
+    const plusButtonEl = getByText("+", { selector: "button" });
+    const twoButtonEl = getByText("2", { selector: "button" });
+    const equalsButtonEl = getByText("=", { selector: "button" });
+
+    fireEvent.click(oneButtonEl);
+    fireEvent.click(plusButtonEl);
+    fireEvent.click(twoButtonEl);
+    fireEvent.click(equalsButtonEl);
+    expect(counterValueEl).toHaveTextContent("3");
 });
